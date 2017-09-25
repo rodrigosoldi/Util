00501dbdc02b86b02345f511296c4023593bd4f2
Rodrigo Soldi <rodrigosoldi@icloud.com>
2017-09-25 11:13:18 -0700

    IsEmail

---
+++ Util/Categories/NSString/NSString+RSEmail.h
@@ -0,0 +1,15 @@
+//
+//  NSString+RSEmail.h
+//  Util
+//
+//  Created by Rodrigo Soldi on 25/09/17.
+//  Copyright © 2017 Rodrigo Soldi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (RSEmail)
+
+- (BOOL)isEmail;
+
+@end
---
+++ Util/Categories/NSString/NSString+RSEmail.m
@@ -0,0 +1,22 @@
+//
+//  NSString+RSEmail.m
+//  Util
+//
+//  Created by Rodrigo Soldi on 25/09/17.
+//  Copyright © 2017 Rodrigo Soldi. All rights reserved.
+//
+
+#import "NSString+RSEmail.h"
+
+@implementation NSString (RSEmail)
+
+- (BOOL)isEmail {
+    BOOL stricterFilter = YES;
+    NSString *stricterFilterString = @"^[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$";
+    NSString *laxString = @"^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$";
+    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:self];
+}
+
+@end
